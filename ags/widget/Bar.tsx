2da7c73575a57b32d15c94dae87992dc0f33a6c5
weoweo <weoweo@WEOHQ.attlocal.net>
2026-08-23 01:29:02 -0500

    added gitignore

--- ags/widget/Bar.tsx
+++ ags/widget/Bar.tsx
@@ -196,7 +196,7 @@ function Battery() {
   )
 }
 
-function Clock({ format = "%H:%M" }) {
+function Clock({ format = "%a %b %d %I:%M:%S %p" }) {
   const time = createPoll("", 1000, () => {
     return GLib.DateTime.new_now_local().format(format)!
   })
